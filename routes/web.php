1610d3e86a17b627c5bc51ef1c33fa65ca37982a
GuillermoDEV51 <guillermodev2@gmail.com>
2025-11-29 23:49:07 -0400

    primeras rutas y controladors

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,10 @@
 <?php
 
+use App\Http\Controllers\LoginController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('Login', function () {
-    return view('welcome');
-});
+Route::get('/login', [LoginController::class, 'index']);
