61da230f67426a2a55f113459fb84cdac3d711fb
Kev1nM4nu <kenken29815793@gmal.com>
2026-01-08 20:26:33 -0400

    Registro arreglado

--- resources/views/Registro.blade.php
+++ resources/views/Registro.blade.php
@@ -82,22 +82,177 @@
             <div class="form-group2">
               <label class="form-label">Fecha de nacimiento</label>
               <div class="date-row">
-                <input type="text" class="form-input" placeholder="Día" required>
-                <input type="text" class="form-input" placeholder="Mes" required>
-                <input type="text" class="form-input" placeholder="Año" required>
+                <div class="form-group2">
+                  <select class="form-input" id="dia" required>
+                    <option value="" disabled selected>Día</option>
+                    <option value="01">01</option>
+                    <option value="02">02</option>
+                    <option value="03">03</option>
+                    <option value="04">04</option>
+                    <option value="05">05</option>
+                    <option value="06">06</option>
+                    <option value="07">07</option>
+                    <option value="08">08</option>
+                    <option value="09">09</option>
+                    <option value="10">10</option>
+                    <option value="11">11</option>
+                    <option value="12">12</option>
+                    <option value="13">13</option>
+                    <option value="14">14</option>
+                    <option value="15">15</option>
+                    <option value="16">16</option>
+                    <option value="17">17</option>
+                    <option value="18">18</option>
+                    <option value="19">19</option>
+                    <option value="20">20</option>
+                    <option value="21">21</option>
+                    <option value="22">22</option>
+                    <option value="23">23</option>
+                    <option value="24">24</option>
+                    <option value="25">25</option>
+                    <option value="26">26</option>
+                    <option value="27">27</option>
+                    <option value="28">28</option>
+                    <option value="29">29</option>
+                    <option value="30">30</option>
+                    <option value="31">31</option>
+                  </select>
+                </div>
+                <div class="form-group2">
+                  <select class="form-input" id="mes" required>
+                    <option value="" disabled selected>Mes</option>
+                    <option value="01">Enero</option>
+                    <option value="02">Febrero</option>
+                    <option value="03">Marzo</option>
+                    <option value="04">Abril</option>
+                    <option value="05">Mayo</option>
+                    <option value="06">Junio</option>
+                    <option value="07">Julio</option>
+                    <option value="08">Agosto</option>
+                    <option value="09">Septiembre</option>
+                    <option value="10">Octubre</option>
+                    <option value="11">Noviembre</option>
+                    <option value="12">Diciembre</option>
+                  </select>
+                </div>
+                <div class="form-group2">
+                  <select class="form-input" id="anio" required>
+                    <option value="" disabled selected>Año</option>
+                    <option value="2025">2025</option>
+                    <option value="2024">2024</option>
+                    <option value="2023">2023</option>
+                    <option value="2022">2022</option>
+                    <option value="2021">2021</option>
+                    <option value="2020">2020</option>
+                    <option value="2019">2019</option>
+                    <option value="2018">2018</option>
+                    <option value="2017">2017</option>
+                    <option value="2016">2016</option>
+                    <option value="2015">2015</option>
+                    <option value="2014">2014</option>
+                    <option value="2013">2013</option>
+                    <option value="2012">2012</option>
+                    <option value="2011">2011</option>
+                    <option value="2010">2010</option>
+                    <option value="2009">2009</option>
+                    <option value="2008">2008</option>
+                    <option value="2007">2007</option>
+                    <option value="2006">2006</option>
+                    <option value="2005">2005</option>
+                    <option value="2004">2004</option>
+                    <option value="2003">2003</option>
+                    <option value="2002">2002</option>
+                    <option value="2001">2001</option>
+                    <option value="2000">2000</option>
+                    <option value="1999">1999</option>
+                    <option value="1998">1998</option>
+                    <option value="1997">1997</option>
+                    <option value="1996">1996</option>
+                    <option value="1995">1995</option>
+                    <option value="1994">1994</option>
+                    <option value="1993">1993</option>
+                    <option value="1992">1992</option>
+                    <option value="1991">1991</option>
+                    <option value="1990">1990</option>
+                    <option value="1989">1989</option>
+                    <option value="1988">1988</option>
+                    <option value="1987">1987</option>
+                    <option value="1986">1986</option>
+                    <option value="1985">1985</option>
+                    <option value="1984">1984</option>
+                    <option value="1983">1983</option>
+                    <option value="1982">1982</option>
+                    <option value="1981">1981</option>
+                    <option value="1980">1980</option>
+                    <option value="1979">1979</option>
+                    <option value="1978">1978</option>
+                    <option value="1977">1977</option>
+                    <option value="1976">1976</option>
+                    <option value="1975">1975</option>
+                    <option value="1974">1974</option>
+                    <option value="1973">1973</option>
+                    <option value="1972">1972</option>
+                    <option value="1971">1971</option>
+                    <option value="1970">1970</option>
+                    <option value="1969">1969</option>
+                    <option value="1968">1968</option>
+                    <option value="1967">1967</option>
+                    <option value="1966">1966</option>
+                    <option value="1965">1965</option>
+                    <option value="1964">1964</option>
+                    <option value="1963">1963</option>
+                    <option value="1962">1962</option>
+                    <option value="1961">1961</option>
+                    <option value="1960">1960</option>
+                    <option value="1959">1959</option>
+                    <option value="1958">1958</option>
+                    <option value="1957">1957</option>
+                    <option value="1956">1956</option>
+                    <option value="1955">1955</option>
+                    <option value="1954">1954</option>
+                    <option value="1953">1953</option>
+                    <option value="1952">1952</option>
+                    <option value="1951">1951</option>
+                    <option value="1950">1950</option>
+                  </select>
+                </div>
               </div>
             </div>
             
             <div class="form-group2">
-              <input type="tel" class="form-input" placeholder="Número de teléfono" required>
+              <input type="tel" class="form-input" 
+                     placeholder="Número de teléfono" 
+                     pattern="[0-9]*" 
+                     inputmode="numeric"
+                     oninput="this.value = this.value.replace(/[^0-9]/g, '')"
+                     maxlength="11"
+                     required>
             </div>
+
+            <div class="form-group2">
+                <select class="form-input" required>
+                  <option value="">Ubicación</option>
+                  <option value="Caracas">Caracas</option>
+                  <option value="Miranda">Miranda</option>
+                  <option value="La Guaira">La Guaira</option>
+                  <option value="Zulia">Zulia</option>
+                  <option value="Lara">Lara</option>
+                  <option value="Carabobo">Carabobo</option>
+                  <option value="Sucre">Sucre</option>
+                  <option value="Anzoátegui">Anzoátegui</option>
+                  <option value="Nueva Esparta">Nueva Esparta</option>                
+                </select>
+              </div>
             
             <div class="form-group2">
-              <input type="text" class="form-input" placeholder="Ubicación" required>
-            </div>
-            
-            <div class="form-group2">
-              <input type="text" class="form-input" placeholder="Cédula" required>
+              <input type="text" class="form-input" 
+                     name="cedula" 
+                     placeholder="Cédula" 
+                     pattern="[0-9]*"
+                     inputmode="numeric"
+                     oninput="this.value = this.value.replace(/[^0-9]/g, '')"
+                     maxlength="8"
+                     required>
             </div>
             
             <div class="form-group2">
