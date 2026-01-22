8c81838374028005d58bac68238ce0aa482fa590
GuillermoDEV51 <guillermodev2@gmail.com>
2026-01-21 23:41:26 -0400

    update

--- app/Http/Controllers/InicioController.php
+++ app/Http/Controllers/InicioController.php
@@ -22,7 +22,7 @@ class InicioController extends Controller
 
         // Manejar la carga de la foto si existe
         if ($request->hasFile('foto')) {
-            $path = $request->file('foto')->store('public/fotos_mascotas');
+            $path = $request->file('foto')->store('mascotas', 'public');
             $data['foto'] = basename($path);
         }
 
@@ -34,7 +34,7 @@ class InicioController extends Controller
 
     }
 
-    
+
     public function index(){
         $especies = \App\Models\Especie::all();
         return view('vistavacia', compact('especies'));
