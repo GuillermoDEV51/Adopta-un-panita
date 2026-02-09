74b60b49a768c61e153bdcf1c09614ada36e0546
Alarconwth <jalxrcon18@gmail.com>
2026-02-09 12:34:34 -0400

    arreglo de css inicio

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -20,7 +20,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600;700;800&family=Pacifico&display=swap"
         rel="stylesheet">
 
-    @vite(['resources/css/inicio.css', 'resources/js/menu.js', 'resources/js/publicar-panita.js', 'resources/css/modal-publicar.css', 'resources/css/mascotas-home.css', 'resources/css/stylessadmin.css'])
+    @vite(['resources/css/inicio.css', 'resources/js/menu.js', 'resources/js/publicar-panita.js', 'resources/css/modal-publicar.css', 'resources/css/mascotas-home.css'])
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
